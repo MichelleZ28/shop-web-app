ae8ad0091dc0b102765f9b39d0ee03de806d636c
haoyue <haoyeuzhou@gmail.com>
2025-11-07 15:47:29 -0600

    Deploy frontend + backend update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/shop-web-app/",
   server: {
     proxy: {
       '/api': {
